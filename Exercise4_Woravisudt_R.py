d77e997d41c15000d4710485fc11be5e6e088dee
mrxtenten <89195655+mrxtenten@users.noreply.github.com>
2021-08-20 12:09:21 +0700

    Create Exercise4_Woravisudt_R.py

---
+++ Exercise4_Woravisudt_R.py
@@ -0,0 +1,10 @@
+FE = 80.0
+GB = 66.5
+ICS = 73.0
+CP = 81.5
+
+print("--- Your Score ---")
+print("Foundation English : ",FE,"คะแนน")
+print("General Business : ",GB,"คะแนน")
+print("Introduction to Computer Systems : ",ICS,"คะแนน")
+print("Computer Programming : ",CP,"คะแนน")
