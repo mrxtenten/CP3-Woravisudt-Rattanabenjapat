eb302638484c1cb0d402ccb35029937c1117f807
mrxtenten <89195655+mrxtenten@users.noreply.github.com>
2021-09-24 09:46:45 +0700

    Update Exercise11_Woravisudt_R.py

--- Exercise11_Woravisudt_R.py
+++ Exercise11_Woravisudt_R.py
@@ -6,3 +6,22 @@ for x in range(number):
     text=text+"*"
     print(gap,text)
     text=text+"*"
+
+# ตัวอย่างของคนอื่น (บอกแล้ววิธีเขียนโปรแกรมมีหลายแบบ)
+""" 
+number = int(input("กรอกตัวเลข : "))
+print("จำนวน",number,"แถว")
+for i in range(number):
+    print(" "*(number-i),"*"*(((i+1)*2)-1))
+"""
+
+"""
+n = int(input())
+space = n-1
+star = 1
+for i in range(n):
+    print(" "*space,end="")
+    print("*"*star)
+    star+=2
+    space-=1
+"""
