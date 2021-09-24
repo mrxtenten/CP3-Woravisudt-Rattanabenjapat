22abb523371e2214f8d6526687b314da595593ff
mrxtenten <89195655+mrxtenten@users.noreply.github.com>
2021-09-24 09:46:55 +0700

    Create Function Param.py

---
+++ Function Param.py
@@ -0,0 +1,13 @@
+def Plus(x,y):
+    print(x+y)
+def Minus(x,y):
+    print(x-y)
+def Multiply(x, y):
+    print(x*y)
+def Divide(x, y):
+    print(x/y)
+
+Plus(10,5)
+Minus(10,5)
+Multiply(10,5)
+Divide(10,5)
